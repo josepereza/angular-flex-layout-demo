0c9e6de86f746f8cbbc2edd6209b59d3351a9b77
Fred Morrison <fpmorrison@verizon.net>
2019-03-29 21:14:58 -0400

    split docs-grid component html into separate file

--- src/app/grid/docs-grid/docs-grid.component.ts
+++ src/app/grid/docs-grid/docs-grid.component.ts
@@ -2,12 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'demo-docs-grid',
-  template: `
-    <demo-grid-layout class="small-demo"></demo-grid-layout>
-    <demo-grid-nested class="small-demo"></demo-grid-nested>
-    <demo-grid-minmax class="small-demo"></demo-grid-minmax>
-    <demo-grid-position class="small-demo"></demo-grid-position>
-    <demo-grid-overlay class="small-demo"></demo-grid-overlay>
-  `
+  templateUrl: './docs-grid.component.html'
 })
 export class DocsGridComponent {}

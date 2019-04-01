47af7c76684a92523caa6371b5c6e915d2bd392d
Fred Morrison <fpmorrison@verizon.net>
2019-04-01 09:52:47 -0400

    removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-
-import { VERSION } from '@angular/flex-layout';
+//
 import { AngularVersionInformationService } from './shared/angular-version-information.service';
 import { MaterialVersionInformationService } from './shared/material-version-information.service';
 import { AngularFLexLayoutVersionInformationService } from './shared/angular-flex-layout-version-information.service';

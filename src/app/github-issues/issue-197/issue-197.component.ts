411596bd180c2f3eeac2933418128981a3959caa
Fred Morrison <fpmorrison@verizon.net>
2019-04-01 17:31:00 -0400

    use my custom ngClass  settings

--- src/app/github-issues/issue-197/issue-197.component.ts
+++ src/app/github-issues/issue-197/issue-197.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'demo-issue-197',
-  templateUrl: './issue-197.component.html'
+  templateUrl: './issue-197.component.html',
+  styleUrls: ['./issue-197.component.scss']
 })
 export class Issue197Component {}
